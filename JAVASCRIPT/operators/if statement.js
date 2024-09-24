de273a2c0ef72f0cea7775860244a7cd1cb99e48
gabrielfemi <gabrielefevberha@gmail.com>
2024-09-24 13:46:22 -0700

    if statement class

---
+++ JAVASCRIPT/operators/if statement.js
@@ -0,0 +1,50 @@
+let age = 18;
+
+if (age >= 18) {
+  console.log("you can enter this site");
+}
+
+let temparature = 20;
+
+if (temparature >= 20) {
+  console.log("you can wear a hoodie");
+}
+
+let name = "gabriel";
+if (name !== "Femi") {
+  console.log("only Femi can access this file");
+}
+
+let score = 42;
+if (score < 40) {
+  console.log(`your score is ${score} you failed`);
+} else {
+  console.log(`your score is ${score} you passed`);
+}
+
+let didAssignment = true;
+if (didAssignment) {
+  console.log("you cannot enter my class");
+} else {
+  console.log("you can enter my class");
+}
+
+let score2 = 60;
+if (score2 >= 70 && score2 <= 100) {
+  console.log("you had an A");
+} else if (score2 >= 60 && score2 <= 100) {
+  console.log("you had a B");
+} else if (score2 >= 50 && score2 <= 100) {
+  console.log("you had a C");
+} else {
+  console.log("ivalid details");
+}
+
+let income = 50000;
+if (income <= 49999) {
+  console.log("Eligible for loan");
+} else if (income <= 30000) {
+  console.log("Eligible for loan");
+} else {
+  console.log("Not Eligible for loan");
+}
