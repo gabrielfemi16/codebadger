bb3098219398d11469ab65c76287bf4d4dab4ab7
gabrielfemi <gabrielefevberha@gmail.com>
2024-09-28 15:23:36 -0700

    add this

---
+++ JAVASCRIPT/if statement2.js
@@ -0,0 +1,31 @@
+let num = 1;
+
+while (num <= 10) {
+  console.log(`${num}`);
+  num++;
+}
+
+let sorry = 1;
+
+while (sorry <= 20) {
+  console.log("i am sorry");
+  sorry++;
+}
+
+let num2 = 1000;
+
+while (num2 <= 1000 && num2 >= 0) {
+  console.log(`${num2}`);
+  num2--;
+}
+
+for (let num2 = 1000; num2 <= 1000 && num2 >= 900; num2--) {
+  console.log(num2);
+}
+
+let num3 = 1;
+
+while (num3 <= 19) {
+  console.log(num3);
+  num3 += 2;
+}
